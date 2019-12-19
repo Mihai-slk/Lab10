aa691030d3cbc3f2eb457b86231f7ce479cc1557
Mihai <sandru.mihai@hotmail.com>
2019-12-19 20:38:30 +0200

    kk

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        int slk;
+                slk = 25;
+                short banii = 15;
+                float datori = 10.5f;
+                float dif = banii - datori;
+                String bugetramas = "dif";
+
+    }
+}
