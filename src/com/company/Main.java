1525e466150e604d812b813a0bb6c6904607fc4d
Mihai <sandru.mihai@hotmail.com>
2020-01-08 18:53:19 +0200

    Buna

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Here. I give you 100 bucks to fuck off!");
         int slk;
                 slk = 25;
                 short banii = 15;
                 float datori = 10.5f;
                 float dif = banii - datori;
                 String bugetramas = "dif";
+                System.out.println(dif);
 
     }
 }
